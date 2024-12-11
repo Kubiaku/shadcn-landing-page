39275fb8fedef298029f828385232daf1104738c
MuTicax <muticax@gmail.com>
2024-12-11 12:03:00 +0100

    x

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,6 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <SponsorsSection />
       <BenefitsSection />
       <FeaturesSection />
       <ServicesSection />
